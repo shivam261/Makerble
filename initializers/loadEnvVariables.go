235d0ce18839f751acc71e80cf0d8e2493423d95
shivam <shivam1705of@gmail.com>
2025-06-20 13:46:14 +0530

    removed bug

--- initializers/loadEnvVariables.go
+++ initializers/loadEnvVariables.go
@@ -8,7 +8,7 @@ import (
 
 func LoadEnvVariables() {
 	// Load environment variables from .env file
-	err := godotenv.Load("production.env")
+	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
