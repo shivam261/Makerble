d41560376d0102120ed9bf58aa335172bb56f3bc
shivam <shivam1705of@gmail.com>
2025-06-21 13:05:55 +0530

    release  mode set up

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -4,6 +4,7 @@ import (
 	"github/shivam261/ClinicManagement/initializers"
 	"github/shivam261/ClinicManagement/middlewares"
 	"github/shivam261/ClinicManagement/routes"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -17,6 +18,11 @@ func init() {
 func main() {
 	// This is the main entry point of the application.
 	// You can add your application logic here.
+	if os.Getenv("GIN_MODE") == "release" {
+		gin.SetMode(gin.ReleaseMode) // Set Gin to release mode for production
+	} else {
+		gin.SetMode(gin.DebugMode) // Set Gin to debug mode for development
+	}
 	router := gin.Default()
 	// Set up middleware
 	router.Use(middlewares.RateLimiter(100, 1*time.Minute)) // Limit to 100 requests per minute
